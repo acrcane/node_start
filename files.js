6eefceb55094cc0c3fcf6b6f466e4f41381eac46
arcane <arcanesir.neo@gmail.com>
2023-11-18 16:22:10 +0100

    first

---
+++ files.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const fs = require('fs').promises;
+
+const createFile = async (fileName, content) => {
+    const file = {
+        
+    }
+}
---
+++ index.js
@@ -0,0 +1,23 @@
+const argv = require('yargs').argv;
+
+
+function invokeAction({ action }) {
+  switch (action) {
+    case '':
+
+      break;
+
+    case '':
+
+      break;
+
+    case '':
+
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
